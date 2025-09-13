f11fe8c4647595eae336c6c1b4b47a8cf17b054a
petherEm <pether.maciejewski@gmail.com>
2025-09-13 16:59:02 +0200

    new in update

--- src/app/(store)/[lang]/(main)/new-in/page.tsx
+++ src/app/(store)/[lang]/(main)/new-in/page.tsx
@@ -3,7 +3,18 @@ import React from "react";
 import { Container } from "@/components/ui/container";
 
 const NewIn = () => {
-  return <Container>NewIn</Container>;
+  return (
+    <Container className="py-16">
+      <div className="space-y-8">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight">New In</h1>
+          <p className="mt-4 text-lg text-muted-foreground">
+            Work in progress...
+          </p>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default NewIn;
